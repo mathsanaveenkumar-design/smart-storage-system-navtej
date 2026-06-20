cfce72763e3cb7ec84d72cd35ef4d2c81c2d93df
mathsanaveenkumar-design <mathsanaveenkumar@gmail.com>
2026-06-20 14:51:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+DEFAULT_MINIMUM_STOCK = 10
+OFFLINE_DB_PATH = "offline_data/offline_queue.db"
+PHOTO_BUCKET = "component-photos"
